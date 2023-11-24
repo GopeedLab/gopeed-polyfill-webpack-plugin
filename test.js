db2b8b9e1baa0ae1b2f13340d722de985488ffbb
liwei <liwei-8466@qq.com>
2023-11-24 16:02:36 +0800

    test: fix

--- test.js
+++ test.js
@@ -14,15 +14,15 @@ test('main', async t => {
 		},
 		plugins: [
 			new GopeedPolyfillPlugin({
-				excludeAliases: ['console'],
+				excludeAliases: ['process'],
 			}),
 		],
 	});
 
 	t.is(require('./dist/1.js'), 'Hello World');
 
-	// https://github.com/browserify/console-browserify/blob/f7eefc7c908c29d2e94954e5c6c1098e8c1028b4/index.js#L63
-	t.false(fs.readFileSync('./dist/1.js').toString().includes('No such label: '));
+	// https://github.com/defunctzombie/node-process/blob/77caa43cdaee4ea710aa14d11cea1705293c0ef3/browser.js#L16C8-L16C9
+	t.false(fs.readFileSync('./dist/1.js').toString().includes('clearTimeout has not been defined'));
 
 	// https://github.com/feross/buffer/blob/master/index.js#L80
 	t.true(fs.readFileSync('./dist/1.js').toString().includes('is invalid for option "size"'));
@@ -39,15 +39,15 @@ test('includeAliases', async t => {
 		},
 		plugins: [
 			new GopeedPolyfillPlugin({
-				includeAliases: ['console'],
+				includeAliases: ['process'],
 			}),
 		],
 	});
 
 	t.is(require('./dist/2.js'), 'Hello World');
 
-	// https://github.com/browserify/console-browserify/blob/f7eefc7c908c29d2e94954e5c6c1098e8c1028b4/index.js#L63
-	t.true(fs.readFileSync('./dist/2.js').toString().includes('No such label: '));
+	// https://github.com/defunctzombie/node-process/blob/77caa43cdaee4ea710aa14d11cea1705293c0ef3/browser.js#L16C8-L16C9
+	t.true(fs.readFileSync('./dist/2.js').toString().includes('clearTimeout has not been defined'));
 
 	// https://github.com/feross/buffer/blob/master/index.js#L80
 	t.false(fs.readFileSync('./dist/2.js').toString().includes('is invalid for option "size"'));
@@ -55,7 +55,7 @@ test('includeAliases', async t => {
 
 test('includeAliases and excludeAliases used at the same time', t => {
 	t.throws(() => new GopeedPolyfillPlugin({
-		includeAliases: ['console'],
+		includeAliases: ['process'],
 		excludeAliases: ['crypto'],
 	}), {instanceOf: Error});
 });
