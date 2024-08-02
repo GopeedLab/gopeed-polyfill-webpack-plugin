41f9ed5a10a6143f8964bfcaf7320e792ad62a30
liwei <liwei-8466@qq.com>
2024-08-02 11:46:34 +0800

    style: missing space

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ module.exports = class GopeedPolyfillPlugin {
 				TextEncoder: [require.resolve('text-encoding-utf-8'), 'TextEncoder'],
 				TextDecoder: [require.resolve('text-encoding-utf-8'), 'TextDecoder'],
 				EventTarget: [require.resolve('event-target-shim'), 'EventTarget'],
-				atob:[require.resolve('base-64'), 'decode'],
-				btoa:[require.resolve('base-64'), 'encode'],
+				atob: [require.resolve('base-64'), 'decode'],
+				btoa: [require.resolve('base-64'), 'encode'],
 			})),
 		);
 
